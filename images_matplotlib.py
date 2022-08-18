f994bf9a2a67e910f1f32f55240a1cabd621cf4b
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-08-18 19:57:21 +0500

    Image Visualization using matplotlib

---
+++ images_matplotlib.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib as mlb
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+##   You can import image using Image method in PIL(Python Imaging Library).
+
+if __name__ == '__main__':
+    """
+    An image loaded using PIL is simply a 3 dimensional numpy array containing pixels intensities for red,
+    green and blue (RGB) channel of the image.
+    """
